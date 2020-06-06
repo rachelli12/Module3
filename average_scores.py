b4ff2f89e9f1371018778ba8558e7ef66ba3af05
rachelli12 <65979017+rachelli12@users.noreply.github.com>
2020-06-06 00:43:27 -0500

    Add files via upload

--- average_scores.py
+++ average_scores.py
@@ -1,2 +1,39 @@
-def average():
-    pass
+"""
+Program: average_scores.py
+Author: Rachel Li
+Last date modified: 06/06/2020
+
+The purpose of this program is to read in one person's name, first and last, age and three scores out of 100.
+The program wants to find the average of the three scores.
+"""
+def average(score1, score2, score3):
+    average_scores = (score1 + score2 + score3) / 3
+    print('average: ', (score1 + score2 + score3) / 3)
+    # declare variables. use score1, score2, score3 in calculation
+    return average_scores
+
+if __name__ == '__main__':
+    #Get input for scores
+    score1 = int(input('score 1: '))
+    score2 = int(input('score 2: '))
+    score3 = int(input('score 3: '))
+
+    #Calculate average scores
+    average_scores = average(score1, score2, score3)
+
+
+    #User will input last name and first name here
+    last_name = input("Last name: ")
+    first_name = input("First name: ")
+
+    #User will input age here
+    age = input("age: ")
+
+    #Print output
+    #lastname, firstname age:? average grade: with .2precision
+    print(f'{last_name}, {first_name} age: {age} average grade: {average_scores:.2f}')
+
+#example output: Roriguez, Linda age: 21 average grade: 92.50
+#Program output: Li, Rachel age: 19 average grade: 93.33
+
+
--- test_average_scores.py
+++ test_average_scores.py
@@ -1,12 +1,22 @@
+"""
+Program: test_average_scores.py
+Author: Rachel Li
+Last date modified: 06/06/2020
+
+The purpose of this program is to test average_scores.py.
+"""
 import unittest
 import unittest.mock as mock
 from format_output import average_scores as avg
 
 class AverageTestCase(unittest.TestCase):
     def test_average(self):
-        with mock.patch ('builtins.input', side_effect=[85,90,95]):
-            assert avg.average() == 90
-
+        with mock.patch('builtins.input', side_effect=[85,90,95]):
+            assert avg.average(85, 90, 95) == 90
 
 if __name__ == '__main__':
     unittest.main()
+
+#The program ran 1 test in 0.001s
+#Test ran OK.
+#Observed output: average: 90.0.
